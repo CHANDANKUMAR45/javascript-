c73dc4064f358a12ec312f29360b35d31d49ed31
CHANDANKUMAR45 <167713141+CHANDANKUMAR45@users.noreply.github.com>
2024-07-25 20:55:22 +0000

    Datatypes Summary

---
+++ 01_basic/datatypes.summary.js
@@ -0,0 +1,36 @@
+//DATA TYPES 
+
+//PRIMITITVE
+//7 TYPES: STRING, NUMBER,BOOLEAN,NULL,UNDEFINED,sYMBOL,BIGINT
+
+const score=100
+const scoreValue=100.3
+
+const isLoggedIn=false
+const outsideTemp=null
+let userEmail;
+
+const id=Symbol('123')
+const anotherId=Symbol('123')
+
+console.log(id==anotherId)
+
+const bigNumber=4654651654
+
+//NON-PRIMITIVE(Refrence)
+// Array,objects,function
+
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+    name: "hitesh",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof anotherId);
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
