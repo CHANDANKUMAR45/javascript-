b9a6cb52ea9a836ba622620b8b018429b50b8298
CHANDANKUMAR45 <167713141+CHANDANKUMAR45@users.noreply.github.com>
2024-07-27 15:07:40 +0000

    date

---
+++ 01_basic/07_datesin.js
@@ -0,0 +1,18 @@
+let myDate=new Date()
+
+//console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toLocaleDateString());
+// console.log(typeof myDate);
+
+// //let myCreateDate=new Date(2023,0,23)
+// let myCreateDate=new Date(2023,0,23,5,3)
+let myCreateDate=new Date("2024-01-14")
+//console.log(myCreateDate.toLocaleString());
+
+
+let myTimeStamp=Date.now()
+
+console.log(myTimeStamp);
+console.log(myCreateDate.getTime());
