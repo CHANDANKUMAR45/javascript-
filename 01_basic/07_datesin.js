3cef5cc96fe39b2f2027f2d879254d75fb810f6e
CHANDANKUMAR45 <167713141+CHANDANKUMAR45@users.noreply.github.com>
2024-07-29 12:59:53 +0000

    Date and Time

--- 01_basic/07_datesin.js
+++ 01_basic/07_datesin.js
@@ -14,5 +14,17 @@ let myCreateDate=new Date("2024-01-14")
 
 let myTimeStamp=Date.now()
 
-console.log(myTimeStamp);
-console.log(myCreateDate.getTime());
+// console.log(myTimeStamp);
+// console.log(myCreateDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate=new Date()
+console.log(newDate);
+console.log(newDate.getMonth()+1);
+console.log(newDate.getDay());
+
+//  `${newDate.getDay()} and the time `
+
+newDate.toLocaleString('defaukt',{
+    weekday:"long",
+})
