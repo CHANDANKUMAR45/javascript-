3d0176178964f702c45b16c7bd78f6c9e64db652
CHANDANKUMAR45 <167713141+CHANDANKUMAR45@users.noreply.github.com>
2024-07-29 15:06:14 +0000

    object

---
+++ 02_basics/03_object.js
@@ -0,0 +1,13 @@
+// singleton
+//object.create
+
+//object literals
+const JsUser={
+    name:"chandan",
+    age:19,
+    location:"Bhilai",
+    email:"chandan@google.com",
+    isLoggedIn:false,
+    lastLoginDays:["Monday","Saturday"]
+    
+}
