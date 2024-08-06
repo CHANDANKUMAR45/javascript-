d3d6b28c104791d6f7da2eb08e3c99851e5d9205
CHANDANKUMAR45 <167713141+CHANDANKUMAR45@users.noreply.github.com>
2024-08-06 21:12:40 +0000

    object

--- 02_basics/03_object.js
+++ 02_basics/03_object.js
@@ -2,8 +2,13 @@
 //object.create
 
 //object literals
+
+
+const mySym=Symbol("key1")
 const JsUser={
     name:"chandan",
+    "full name":"Chandan kumar",
+    [mySym]:"mykey1",
     age:19,
     location:"Bhilai",
     email:"chandan@google.com",
@@ -11,3 +16,28 @@ const JsUser={
     lastLoginDays:["Monday","Saturday"]
     
 }
+
+
+//  console.log(JsUser.email)
+//  console.log(JsUser["email"])
+//  console.log(JsUser["full name"])
+//  console.log(JsUser[mySym])
+
+ JsUser.email="chandan@chatgpt.com"   //update
+ //Object.freeze(JsUser)    //no any one can change the value
+ JsUser.email="chandan@microsoft.com"  
+
+ //console.log(JsUser);
+
+ 
+ JsUser.greeting=function(){
+    console.log("Hello JS user");
+ }
+
+ JsUser.greetingTwo=function(){
+    console.log(`Hello JS user ,${this.name}`);
+ }
+
+ console.log(JsUser.greeting());
+ console.log(JsUser.greetingTwo());
+ 
