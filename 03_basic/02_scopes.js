666cd40b28420de142eb152ff032d97561574c7b
CHANDANKUMAR45 <167713141+CHANDANKUMAR45@users.noreply.github.com>
2024-08-10 20:18:27 +0000

    function and scope

--- 03_basic/02_scopes.js
+++ 03_basic/02_scopes.js
@@ -4,13 +4,62 @@ let a = 300
 if (true) {
     let a = 10
     const b = 20
+    // var c=30
     // console.log("INNER: ", a);
     
 }
 
-
-
-// console.log(a);
+ //console.log(a);
 // console.log(b);
 // console.log(c);
 
+//**********************************LECTURE NO:-22*********** */
+// scope level 
+
+function one()
+{
+    const username="chandan"
+     
+    function two()
+    {
+        const website="youtube"
+        console.log(username);
+        console.log(website);
+    }
+   
+    two()
+    
+}
+//one()
+
+if(true)
+{
+    const username="hitesh"
+    if(username==="hitesh")
+    {
+        const website=" youtube"
+        //console.log(username+website);
+        
+    }
+   // console.log(website);
+    
+}
+
+//console.log(username);
+
+//************************ interesting********* */
+
+console.log(addone(5));
+
+
+function addone(num) {
+    return num+1
+}
+
+//addTwo(5)
+
+const addTwo=function(num) {
+    return num+2
+}
+
+addTwo(5)
