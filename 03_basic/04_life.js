1b778701f5d4c81ee14116514845e3d1000811f8
CHANDANKUMAR45 <167713141+CHANDANKUMAR45@users.noreply.github.com>
2024-08-12 21:38:18 +0000

    life

---
+++ 03_basic/04_life.js
@@ -0,0 +1,12 @@
+//Immediately Invoked Function Expression(LIFE)
+
+(function chai()//named LIFE
+{
+    console.log(`DB CONNECTED`);
+    
+})();
+
+((name)=>{//simpLe LIFE
+    console.log(`DB CONNECTED TWO ${name}`);
+    
+})("chandan")
